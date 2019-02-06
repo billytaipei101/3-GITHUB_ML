d728e6cb876eba07738373bb3be2470f6641a6ad
William Cruz <wi.cruzm@gmail.com>
2019-02-06 19:27:05 +0800

    modified script ch1

--- ArtificialNetworksBasics.m
+++ ArtificialNetworksBasics.m
@@ -1,19 +1,24 @@
 %% LINEAR FUNCTION
 %   This activation function's output is equal to its input
-x = -5:0.1:5;
-y = 0:1:length(x)-1;
+x = 5;
+x = -(x):0.1:x;
+y = x;
 plot(y,x);
+%title('Linear Activation Function','Fontsize',18)
 xlabel('x','FontSize',16) ; ylabel('Linear (x)','FontSize',16);
+
 
 %% LOGISTIC FUNCTION
 %   This activations function has a sigmoid shape
-x = -5:0.1:5;
+x = 5;
+x = -(x):0.1:x;
 y = zeros(1,length(x));
 for n = 1:length(y)
     y(n)=1/(1+exp(1)^(-(x(n))));
 end
 plot(x,y)
-xlabel('x','FontSize',16) ; ylabel('Logistic(x)','FontSize',16);
+%title('Logistic Activation Function','Fontsize',18)
+xlabel('x','FontSize',16) ; ylabel('Logistic (x)','FontSize',16);
 
 %% example
 x = [1 3 5] ; w = [0.25 -0.25 0.30] ; b = -0.5;
